4ef5ae3cc5120de7856323a892472b56dc57ca57
ramesh <lrsramesh13@gmail.com>
2021-10-25 22:22:40 +0530

    add password hashing and Fixed register issues

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.schema";
 import { login } from "../services/user.service";
+import bcrypt from "bcryptjs";
 
 let response = {
     status: false,
@@ -13,14 +14,18 @@ export const createUser = async (req, res) => {
 
         if (!user) throw new Error();
 
-        User.create(user).then(async (user) => {
+        const salt = bcrypt.genSaltSync(10);
+        const passwordHash = bcrypt.hashSync(user.password, salt);
+        user.password = passwordHash;
 
-            await login(user);
-            response.status = true;
-            response.message = 'Registered successfully!';
-            response.response = user;
+        User.create(user).then(async (createdUser) => {
 
-            res.status(201).send(response);
+            login(createdUser).then((updatedUser) => {
+                response.status = true;
+                response.message = 'Registered successfully!';
+                response.response = updatedUser;
+                res.status(201).send(response);
+            }).catch(() => {throw new Error()})
 
         }).catch((err) => {
             response.message = 'Registration failed!';
@@ -56,11 +61,34 @@ export const deleteUser = async () => {
     }
 }
 
-export const loginUser = async () => {
+export const loginUser = async (req, res) => {
     try {
-        
+        const { email, password } = req.body;
+
+        const user = User.findOne({email: email});
+
+        if (!user) throw new Error();
+
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if(!checkPassword) {
+            response.message = 'Invalid login credentials';
+            response['response'];
+            return res.status(401).send(response);
+        }
+
+        await login(user).then((updatedUser) => {
+            response.status = true;
+            response.message = 'Logged in successfully!';
+            response.response = updatedUser;
+            res.status(201).send(response);
+        }).catch((err) => {
+            throw new Error();
+        })
+
     } catch (error) {
-        
+        console.log(error);
+        res.status(400).send('Error in Login!');
     }
 }
 
--- services/user.service.js
+++ services/user.service.js
@@ -1,7 +1,11 @@
 import { generateToken } from "../helpers/generateToken";
 import User from "../models/user.schema";
 
-export const login = async (user) => {
-    user.token = await generateToken(user.email);
-    return await User.updateOne({ email: user.email }, user);
+export const login = (user) => {
+    return new Promise(async (resolve, reject) => {
+        user.token = await generateToken(user.email);
+        const updatedUsert = User.findOneAndUpdate({ email: user.email }, user, {new: true}).select({password: 0});
+        if(updatedUsert) resolve(updatedUsert);
+        reject();
+    })
 }
