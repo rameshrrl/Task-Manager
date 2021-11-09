70ef6823f0b9821ab0af7abe6b607fcec39a43e3
ramesh <lrsramesh13@gmail.com>
2021-11-09 19:36:21 +0530

    Authorization added to handle CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const database = process.env.DATABSE;
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
