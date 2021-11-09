866f6d3ba9a1d1df97de3e7b71e44ff1c6533eb0
ramesh <lrsramesh13@gmail.com>
2021-11-09 19:11:33 +0530

    Fixes for CORS

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const database = process.env.DATABSE;
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
 });
+
 app.use('/', applicationRouter);
 
 mongoose.connect(database, dep)
