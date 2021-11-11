9293a6c4b2a3b1c968cbd8bd05f35a1d145740d1
ramesh <lrsramesh13@gmail.com>
2021-11-11 19:51:11 +0530

    Response tests added

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,17 @@
 import { generateToken } from "../helpers/generateToken";
+import { generateResponse } from "../helpers/response";
 
-test('generateToken', () => {
-    const data = await generateToken('sample@gmail.com');
-    expect(data).toBe(String);
+test('generateToken', async () => {
+    const token = await generateToken('sample@gmail.com');
+    expect(token).toReturn(String);
+})
+
+test('generate success response', () => {
+    const response = generateResponse('success', true, {});
+    expect(response.status).toBeTruthy();
+})
+
+test('generate failure response', () => {
+    const response = generateResponse('failed');
+    expect(response.status).toBeFalsy();
 })
